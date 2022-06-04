28663ee1bb0ae46a8d5ce3557f2e4fefe3b3e267
juanj01707 <90587297+juanj01707@users.noreply.github.com>
2022-06-04 12:00:59 -0500

    Dependencias
    Creamos las dependencias del proyecto

---
+++ ApiMovies/src/test/java/co/movies/api/init/ApiMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.movies.api.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
