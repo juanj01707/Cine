4d00dcfa88f4d7a325d3209eece6eb235c70d798
juanj01707 <90587297+juanj01707@users.noreply.github.com>
2022-06-07 13:20:38 -0500

    AzureSql
    cree el azureSql

--- DataMovies/src/co/movies/data/factory/azuresql/AzureSqlDAOFactory.java
+++ DataMovies/src/co/movies/data/factory/azuresql/AzureSqlDAOFactory.java
@@ -1,5 +1,52 @@
 package co.movies.data.factory.azuresql;
 
-public class AzureSqlDAOFactory {
+import java.sql.Connection;
+
+import co.movies.data.dao.SaleDAO;
+import co.movies.data.factory.DAOFactory;
+
+public class AzureSqlDAOFactory extends DAOFactory {
+
+	@Override
+	protected void openConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Connection getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closeConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void commitTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rollbackTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SaleDAO getSaleDAO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
